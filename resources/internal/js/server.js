5cb3432747718d006b3cd11ec0dc106335d52be7
ejgenc@gmail.com <ejgenc@gmail.com>
2022-03-30 15:38:55 +0200

    Started implementing 'server.js' which serves as a minimal server for color palette processing.

--- resources/internal/js/server.js
+++ resources/internal/js/server.js
@@ -1,1 +1,41 @@
-console.log("server.js imported!");
+"use strict";
+
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const server = http.createServer((request, response) => {
+    if (request.method === "POST") {
+        let body = "";
+
+        // stream the body data 
+        request.on("data", chunk => {
+            body += chunk.toString();
+        });
+
+        // process data here
+        for (let char of body) {
+            body += "1";
+        }
+
+        // when data is done being processed
+        request.on("end", () => {
+            response.writeHead("200");
+            response.end(body);
+        });
+
+        // at the end
+        response.writeHead("200");
+        // return a formatted list of hexcodes
+        response.end(body);
+    }
+    else {
+        response.writeHead("501");
+        response.end();
+    }
+})
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
