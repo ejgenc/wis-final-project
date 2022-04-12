be09de3cee028fd3105d2924e3b974fe9add8aed
ejgenc@gmail.com <ejgenc@gmail.com>
2022-04-12 22:42:48 +0200

    Major work on creating the infrastructure for calling app.py as a subprocess.

--- src/app/app.py
+++ src/app/app.py
@@ -1,7 +1,12 @@
+import sys
+import json
 from urllib.request import urlopen
 
-import numpy as np
-from skimage import color, util
-from sklearn import cluster
+# import numpy as np
+# from skimage import color, util
+# from sklearn import cluster
+
+# body = json.load(sys.stdin())
+# print(body)
 
 print("I am the color palette extraction app!")
